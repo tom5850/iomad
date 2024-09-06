b3ca74ebf83c8345697eebd69a157d3887ba564b
Mairi MacKinnon <mairi@e-learndesign.co.uk>
2024-09-06 13:32:54 +0100

    IOMAD: Added private/public setting for training locations so they can be used with training events on shared courses.

--- mod/trainingevent/version.php
+++ mod/trainingevent/version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->release  = '4.3.5 (Build: 20240610)'; // Human-friendly version name
 $plugin->component  = 'mod_trainingevent';
-$plugin->version  = 2024030100;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2024090600;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2019052000;  // Requires this Moodle version.
 $plugin->cron     = 0;           // Period for cron to check this module (secs).
