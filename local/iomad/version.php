ce8c8a76554fbe9a9d484c2978bea9aabbbf4832
Derick Turner <derick@e-learndesign.co.uk>
2024-10-02 11:56:12 +0100

    IOMAD: Added company, company users, company licenses, company license users, iomad course settings, company user course completion data sources to report builder

--- local/iomad/version.php
+++ local/iomad/version.php
@@ -24,4 +24,4 @@
 $plugin->release  = '4.3.5 (Build: 20240610)'; // Human-friendly version name
 $plugin->component  = 'local_iomad';
 $plugin->requires = 2019052000;   // Requires this Moodle version.
-$plugin->version  = 2024090400;   // The (date) version of this plugin.
+$plugin->version  = 2024090401;   // The (date) version of this plugin.
