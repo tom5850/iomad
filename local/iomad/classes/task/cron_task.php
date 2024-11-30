d9b702c11a386aca9a553943fa66dcbfe9cd4078
Derick Turner <derick@e-learndesign.co.uk>
2024-11-30 03:41:56 +0000

    IOMAD: Reusable licenses should have their allocations cleared down on expiry rather than marked as complete - closes #1973

--- local/iomad/classes/task/cron_task.php
+++ local/iomad/classes/task/cron_task.php
@@ -166,7 +166,7 @@ class cron_task extends \core\task\scheduled_task {
 
         // Clear users from courses where the license has expired and the option is chosen
         mtrace ("Clear users from courses where the license has expired and the option is chosen");
-        if ($userlicenses = $DB->get_records_sql("SELECT clu.* FROM {companylicense_users} clu
+        if ($userlicenses = $DB->get_records_sql("SELECT clu.*,cl.type FROM {companylicense_users} clu
                                                   JOIN {companylicense} cl on (clu.licenseid = cl.id)
                                                   WHERE cl.clearonexpire = 1
                                                   AND cl.cutoffdate < :time
@@ -188,6 +188,10 @@ class cron_task extends \core\task\scheduled_task {
                         foreach ($litrecs as $litrec) {
                             \company_user::delete_user_course($userlicense->userid, $userlicense->licensecourseid, 'autodelete', $litrec->id);
                         }
+                    }
+                    // If this is a re-usable license we want to dump the allocation record too.
+                    if ($userlicense->type == 1 || $userlicense->type ==3) {
+                        $DB->delete_records('companylicense_users', ['id' => $userlicense->id]);
                     }
                 } else {
                     $DB->delete_records('companylicense_users', array('id' => $userlicense->id));
