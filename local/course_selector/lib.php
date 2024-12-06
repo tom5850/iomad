fa0573bc241baa68100079ae47ff844c348c9c2f
Derick Turner <derick@e-learndesign.co.uk>
2024-12-06 22:54:01 +0000

    IOMAD: Change course enrolments for a user gives error - context_company class doesn't exist - closes #2098

--- local/course_selector/lib.php
+++ local/course_selector/lib.php
@@ -470,8 +470,8 @@ abstract class course_selector_base {
         }
 
         // Add some additional sensible conditions.
-        if (!iomad::has_capability('moodle/course:viewhiddencourses', context_system::instance()) &&
-            !iomad::has_capability('moodle/course:viewhiddencourses', context_company::instance(iomad::get_my_companyid(context_system::instance())))) {
+        if (!iomad::has_capability('moodle/course:viewhiddencourses', \core\context\company::instance()) &&
+            !iomad::has_capability('moodle/course:viewhiddencourses', \core\context\company::instance(iomad::get_my_companyid(context_system::instance())))) {
             $tests[] = $u . 'visible = 1';
         }
 
