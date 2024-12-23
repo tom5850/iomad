288acfd91b3aa50a2f3b8b74878df8324b49056f
Derick Turner <derick@e-learndesign.co.uk>
2024-12-23 20:42:15 +0000

    IOMAD: getting a log of deprecated is_dir warnings from missing plugins - closes #2122

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1048,7 +1048,7 @@ class plugin_manager {
             }
 
             // Make sure we have write access to all the existing code.
-            if (is_dir($localinfo->rootdir)) {
+            if (!empty($localinfo->rootdir) && is_dir($localinfo->rootdir)) {
                 if (!$this->is_plugin_folder_removable($component)) {
                     $reason = 'notwritableplugin';
                     return false;
