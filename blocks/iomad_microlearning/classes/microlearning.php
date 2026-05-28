bb2c9edb2bfd031c44d3eb6fa5b4257fe965eeea
tom5850 <thomas.schlienger@treesolution.com>
2026-05-28 10:54:16 +0200

    TSA internal: update microlearning to send reminders only if due date is not passed

--- blocks/iomad_microlearning/classes/microlearning.php
+++ blocks/iomad_microlearning/classes/microlearning.php
@@ -1437,8 +1437,9 @@ class microlearning {
                  mtu.reminder1_date < mtu.due_date
                  OR mtu.due_date = 0
              )
-             AND mtu.reminder1_date < :runtime",
-            ['runtime' => $runtime])) {
+             AND mtu.reminder1_date < :runtime
+             AND (mtu.due_date > :runtime2 OR mtu.due_date = 0)", //THOMAS, 27.5.26: send only if due date is not passed
+            ['runtime' => $runtime, 'runtime2' => $runtime])) {
             foreach ($reminder1users as $reminder1user) {
                 $reminder1user->reminder1_delivered = true;
 
@@ -1492,8 +1493,9 @@ class microlearning {
                  mtu.reminder2_date < mtu.due_date
                  OR mtu.due_date = 0
              )
-             AND mtu.reminder2_date < :runtime",
-            ['runtime' => $runtime])) {
+             AND mtu.reminder2_date < :runtime
+             AND (mtu.due_date > :runtime2 OR mtu.due_date = 0)", //THOMAS, 27.5.26: send only if due date is not passed
+            ['runtime' => $runtime, 'runtime2' => $runtime])) {
             foreach ($reminder2users as $reminder2user) {
                 $reminder2user->reminder2_delivered = true;
                 $reminder2user->reminder1_delivered = true;
